6b2a494d2cebe3f03bb9024827d5f0648ee41fa2
Daniel Guardia <daniguardiamail@gmail.com>
2020-02-26 23:12:51 +0100

    Sumar finalizado

---
+++ sumador/src/sumador/dgmFunciona.java
@@ -0,0 +1,15 @@
+package sumador;
+
+public class dgmFunciona {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		ASumar s = new ASumar();
+		System.out.println(s.mostrar("5"));
+		System.out.println(s.mostrar("-7"));
+		System.out.println(s.mostrar("3433"));
+		
+
+	}
+
+}
