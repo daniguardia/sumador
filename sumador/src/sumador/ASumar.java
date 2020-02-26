5680fa2693445559af9414c07630f2e41e7ba2f1
Daniel Guardia <daniguardiamail@gmail.com>
2020-02-26 22:45:37 +0100

    segunda prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,11 +1,20 @@
 package sumador;
 
 public class ASumar {
+	
+	public static void main(String [] args) {
+		
+	}
+	
 	/** 
-	 * 
+	 * Metodo que recibe un String numerico y devuelve la suma de los caracteres
+	 * Si es negativo no devuelve nada
+	 * @param string cadena a convertir
 	 */
 	public String mostrar(String string) {
 		// TODO Apéndice de método generado automáticamente
+		if (string.charAt(0)=='-')
+			return null;
 		return (string + " = " + string);
 	}
 
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -1,16 +1,30 @@
 package sumador;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class TestASumar {
 	
+	/**
+	 * Prueba que envia una cadena con un digito
+	 */
     @Test
     public void testUnaCifra() {
         ASumar sumi = new ASumar();
         String resultado = sumi.mostrar("5");
         assertEquals("5 = 5",resultado );
+    }
+    
+    /**
+     * Prueba que envia un negativo
+     */
+    @Test
+    public void testNegativo() {
+    	ASumar sumi= new ASumar();
+    	String resultado = sumi.mostrar("-5");
+    	assertNull(resultado);
     }
 
 }
