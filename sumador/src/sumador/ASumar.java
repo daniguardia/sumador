b4c71261addd51797685a73b10399c3f0f8f69ea
Daniel Guardia <daniguardiamail@gmail.com>
2020-02-26 22:28:06 +0100

    Primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,17 @@
 package sumador;
 
 public class ASumar {
+	/** 
+	 * 
+	 */
+	public String mostrar(String string) {
+		// TODO Apéndice de método generado automáticamente
+		return (string + " = " + string);
+	}
+
+
+
+
+    
 
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestASumar {
-
-	@Test
-	void test() {
-		fail("No implementado aun");
-	}
+	
+    @Test
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.mostrar("5");
+        assertEquals("5 = 5",resultado );
+    }
 
 }
